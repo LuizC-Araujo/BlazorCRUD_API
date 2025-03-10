59e2957711223ecb21b7d39ff0ab531016acbe3a
Luiz Araujo <arauldesign@gmail.com>
2025-03-10 19:15:47 -0300

    feature: adding client dto

---
+++ API/Models/ClientDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class ClientDTO
+    {
+        [Required(ErrorMessage = "Primeiro nome é obrigatório")]
+        public string FirstName { get; set; } = "";
+
+        [Required(ErrorMessage = "Último nome é obrigatório")]
+        public string LastName { get; set; } = "";
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Phone]
+        public  string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string Status { get; set; } = ""; //New, permanent, occasional, inactive
+    }
+
+}
