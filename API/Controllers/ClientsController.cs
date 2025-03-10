9e4e0a282f7ad52efe3f8e28a8357e54894b5671
Luiz Araujo <arauldesign@gmail.com>
2025-03-10 19:27:26 -0300

    feature: update client

--- API/Controllers/ClientsController.cs
+++ API/Controllers/ClientsController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
     public class ClientsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        public ClientsController(ApplicationDbContext context) 
+        public ClientsController(ApplicationDbContext context)
         {
             _context = context;
         }
@@ -56,6 +56,33 @@ namespace API.Controllers
             };
 
             _context.Clients.Add(client);
+            _context.SaveChanges();
+
+            return Ok(client);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditClient(int id, ClientDTO clientDTO)
+        {
+            var checkClientEmail = _context.Clients.FirstOrDefault(x => x.Email == clientDTO.Email);
+            if (checkClientEmail is not null)
+            {
+                ModelState.AddModelError("Email", "O Email já está em uso");
+                var validation = new ValidationProblemDetails(ModelState);
+                return BadRequest(validation);
+            }
+
+            var client = _context.Clients.Find(id);
+            if (client == null)
+                return NotFound();
+
+            client.FirstName = clientDTO.FirstName;
+            client.LastName = clientDTO.LastName;
+            client.Email = clientDTO.Email;
+            client.PhoneNumber = clientDTO.Phone ?? "";
+            client.Address = clientDTO.Address ?? "";
+            client.Status = clientDTO.Status;
+
             _context.SaveChanges();
 
             return Ok(client);
