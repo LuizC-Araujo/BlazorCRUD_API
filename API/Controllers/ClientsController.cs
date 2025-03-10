a5709728930c96b51d1e81965fff182631cd348b
Luiz Araujo <arauldesign@gmail.com>
2025-03-09 21:18:59 -0300

    feature: adding get clients route

---
+++ API/Controllers/ClientsController.cs
@@ -0,0 +1,24 @@
+﻿using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ClientsController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Client> GetClients()
+        {
+            return _context.Clients.OrderByDescending(x => x.Id).ToList();
+        }
+    }
+}
